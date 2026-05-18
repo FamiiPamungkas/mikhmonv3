5bbf0cdff8e1cdce8013a49058e28c2a324524b9
Fahmi Pamungkas <fame1302@gmail.com>
2026-05-18 15:49:43 +0700

    done generate user 2

--- hotspot/generateuser2.php
+++ hotspot/generateuser2.php
@@ -25,98 +25,32 @@ require_once __DIR__ . '/../user-manager/utils.php';
 if (!isset($_SESSION["mikhmon"])) {
 	header("Location:../admin.php?id=login");
 } else {
-// time zone
-date_default_timezone_set($_SESSION['timezone']);
-
-	$genprof = $_GET['genprof'];
-	if ($genprof != "") {
-		$getprofile = $API->comm("/ip/hotspot/user/profile/print", array(
-			"?name" => "$genprof",
-		));
-		$ponlogin = $getprofile[0]['on-login'];
-		$getprice = explode(",", $ponlogin)[2];
-		if ($getprice == "0") {
-			$getprice = "";
-		} else {
-			$getprice = $getprice;
-		}
-
-		$getvalid = explode(",", $ponlogin)[3];
-
-		$getlocku = explode(",", $ponlogin)[6];
-		if ($getlocku == "") {
-			$getprice = "Disable";
-		} else {
-			$getlocku = $getlocku;
-		}
-
-		if ($currency == in_array($currency, $cekindo['indo'])) {
-			$getprice = $currency . " " . number_format((float)$getprice, 0, ",", ".");
-		} else {
-			$getprice = $currency . " " . number_format((float)$getprice);
-		}
-		$ValidPrice = "<b>Validity : " . $getvalid . " | Price : " . $getprice . " | Lock User : " . $getlocku . "</b>";
-	} else {
-	}
+    // time zone
+    date_default_timezone_set($_SESSION['timezone']);
 
 	$srvlist = $API->comm("/ip/hotspot/print");
     $profiles = userman("profiles");
 
 	if (isset($_POST['qty'])) {
 		$qty = ($_POST['qty']);
-		$server = ($_POST['server']);
 		$user = ($_POST['user']);
-		$userl = ($_POST['userl']);
-		$prefix = ($_POST['prefix']);
-		$char = ($_POST['char']);
 		$profile = ($_POST['uprofile']);
-		$timelimit = ($_POST['timelimit']);
-		$datalimit = ($_POST['datalimit']);
+		$prefix = ($_POST['prefix']);
 		$adcomment = ($_POST['adcomment']);
-		$mbgb = ($_POST['mbgb']);
-		if ($timelimit == "") {
-			$timelimit = "0";
-		} else {
-			$timelimit = $timelimit;
-		}
-		if ($datalimit == "") {
-			$datalimit = "0";
-		} else {
-			$datalimit = $datalimit * $mbgb;
-		}
+
 		if ($adcomment == "") {
 			$adcomment = "";
-		} else {
-			$adcomment = $adcomment;
 		}
 
         $config = $profiles[$profile];
 		$getprofile = $API->comm("/ip/hotspot/user/profile/print", array("?name" => $config['hotspot_profile']));
-		$ponlogin = $getprofile[0]['on-login'];
-		$getvalid = explode(",", $ponlogin)[3];
-		$getprice = explode(",", $ponlogin)[2];
-		$getsprice = explode(",", $ponlogin)[4];
-		$getlock = explode(",", $ponlogin)[6];
-		$_SESSION['ubp'] = $profile;
-		$commt = $user . "-" . rand(100, 999) . "-" . date("ymd") . "-" . $adcomment;
-		$gentemp = $commt . "|~" . $profile . "~" . $getvalid . "~" . $getprice . "!".$getsprice."~" . $timelimit . "~" . $datalimit . "~" . $getlock;
-		$gen = '<?php $genu="'.encrypt($gentemp).'";?>';
-		$temp = './voucher/temp.php';
-		$handle = fopen($temp, 'w') or die('Cannot open file:  ' . $temp);
-		$data = $gen;
-		fwrite($handle, $data);
+        $hotspot_profile = $config['hotspot_profile'];
+        if (!$getprofile){
+            $hotspot_profile = "default";
+        }
 
-		$a = array("1" => "", "", 1, 2, 2, 3, 3, 4);
+		$comment = $user . "-" . rand(100, 999) . "-" . date("ymd") . "-" . $adcomment;
 
-
-        error_log("PROFILE ".print_r($config,true));
-        error_log("GET PROFILE  ".print_r($getprofile,true));
-        error_log("ON LOGIN STR ".print_r($ponlogin,true));
-        error_log("ON LOGIN  ".print_r(explode(",",$ponlogin),true));
-        error_log("VALID  ".print_r($getvalid,true));
-        error_log("PRICE  ".print_r($getprice,true));
-        error_log("SALES PRICE  ".print_r($getsprice,true));
-        error_log("LOCK  ".print_r($getlock,true));
 		if ($config) {
             $u = [];
             $p = [];
@@ -126,74 +60,19 @@ date_default_timezone_set($_SESSION['timezone']);
 			}
 
 			for ($i = 1; $i <= $qty; $i++) {
-//				$API->comm("/ip/hotspot/user/add", array(
-//					"server" => "all",
-//					"name" => "$u[$i]",
-//					"password" => "$p[$i]",
-//					"profile" => $config['hotspot-profile'],
-//					"limit-uptime" => "$timelimit",
-//					"limit-bytes-total" => "$datalimit",
-//					"comment" => "$commt",
-//				));
+				$API->comm("/ip/hotspot/user/add", array(
+					"server" => "all",
+					"name" => "$u[$i]",
+					"password" => "$p[$i]",
+					"profile" => $hotspot_profile,
+					"limit-uptime" => $config['time_limit'],
+					"limit-bytes-total" => "0",
+					"comment" => "$comment",
+				));
 			}
 		}
 
-//		if ($qty < 2) {
-//			echo "<script>window.location='./?hotspot-user=" . $u[1] . "&session=" . $session . "'</script>";
-//		} else {
-//			echo "<script>window.location='./?hotspot-user=generate&session=" . $session . "'</script>";
-//		}
-	}
-
-	$getprofile = $API->comm("/ip/hotspot/user/profile/print");
-	include_once('./voucher/temp.php');
-	$genuser = explode("-", decrypt($genu));
-	$genuser1 = explode("~", decrypt($genu));
-	$umode = $genuser[0];
-	$ucode = $genuser[1];
-	$udate = $genuser[2];
-	$uprofile = $genuser1[1];
-	$uvalid = $genuser1[2];
-	$ucommt = $genuser[3];
-	if ($uvalid == "") {
-		$uvalid = "-";
-	} else {
-		$uvalid = $uvalid;
-	}
-	$uprice = explode("!",$genuser1[3])[0];
-	if ($uprice == "0") {
-		$uprice = "-";
-	} else {
-		$uprice = $uprice;
-	}
-	$suprice = explode("!",$genuser1[3])[1];
-	if ($suprice == "0") {
-		$suprice = "-";
-	} else {
-		$suprice = $suprice;
-	}
-	$utlimit = $genuser1[4];
-	if ($utlimit == "0") {
-		$utlimit = "-";
-	} else {
-		$utlimit = $utlimit;
-	}
-	$udlimit = $genuser1[5];
-	if ($udlimit == "0") {
-		$udlimit = "-";
-	} else {
-		$udlimit = formatBytes($udlimit, 2);
-	}
-	$ulock = $genuser1[6];
-	//$urlprint = "$umode-$ucode-$udate-$ucommt";
-	$urlprint = explode("|", decrypt($genu))[0];
-	if ($currency == in_array($currency, $cekindo['indo'])) {
-		$uprice = $currency . " " . number_format((float)$uprice, 0, ",", ".");
-		$suprice = $currency . " " . number_format((float)$suprice, 0, ",", ".");
-	} else {
-		$uprice = $currency . " " . number_format((float)$uprice);
-		$suprice = $currency . " " . number_format((float)$suprice);
-
+        echo "<script>window.location='./?hotspot=users&comment=$comment&session=" . $session . "'</script>";
 	}
 
 }
@@ -208,6 +87,7 @@ date_default_timezone_set($_SESSION['timezone']);
 	<div class="card-body">
 <form autocomplete="off" method="post" action="">
     <input type="hidden" name="user" value="up">
+    <input type="hidden" name="server" value="all"/>
 	<div>
         <?php if ($_SESSION['ubp'] != "") {
             echo "    <a class='btn bg-warning' href='./?hotspot=users&profile=" . $_SESSION['ubp'] . "&session=" . $session . "'> <i class='fa fa-close'></i> ".$_close." X</a>";
@@ -217,20 +97,11 @@ date_default_timezone_set($_SESSION['timezone']);
             echo "    <a class='btn bg-warning' href='./?hotspot=users&profile=all&session=" . $session . "'> <i class='fa fa-close'></i> ".$_close." Z</a>";
         } ?>
         <button type="submit" name="save" onclick="loader()" class="btn bg-primary" title="Generate User"> <i class="fa fa-save"></i> <?= $_generate ?></button>
-        <a class="btn bg-secondary" title="Print Default" href="./voucher/print.php?id=<?= $urlprint; ?>&qr=no&session=<?= $session; ?>" target="_blank"> <i class="fa fa-print"></i> <?= $_print ?></a>
-        <a class="btn bg-danger" title="Print QR" href="./voucher/print.php?id=<?= $urlprint; ?>&qr=yes&session=<?= $session; ?>" target="_blank"> <i class="fa fa-qrcode"></i> <?= $_print_qr ?></a>
-        <a class="btn bg-info" title="Print Small" href="./voucher/print.php?id=<?= $urlprint; ?>&small=yes&session=<?= $session; ?>" target="_blank"> <i class="fa fa-print"></i> <?= $_print_small ?></a>
     </div>
 <table class="table">
   <tr>
     <td class="align-middle"><?= $_qty ?></td><td><div><input class="form-control " type="number" name="qty" min="1" max="500" value="1" required="1"></div></td>
   </tr>
-    <tr>
-        <td class="align-middle"><label for="server">Server</label></td>
-        <td>
-            <input id="server" type="text" class="form-control" name="server" value="all" readonly/>
-        </td>
-    </tr>
     <tr>
         <td class="align-middle"><label for="uprofile">Profile</label></td>
         <td>
@@ -248,73 +119,12 @@ date_default_timezone_set($_SESSION['timezone']);
 	<tr>
     <td class="align-middle"><?= $_comment ?></td><td><input class="form-control " type="text" title="No special characters" id="comment" autocomplete="off" name="adcomment" value=""></td>
   </tr>
-   <tr >
-    <td  colspan="4" class="align-middle w-12"  id="GetValidPrice">
-    	<?php if ($genprof != "") {
-					echo $ValidPrice;
-				} ?>
-    </td>
-  </tr>
 </table>
 </form>
 </div>
 </div>
 </div>
-
-<div class="col-4">
-	<div class="card">
-		<div class="card-header">
-			<h3><i class="fa fa-ticket"></i> <?= $_last_generate ?></h3>
-		</div>
-		<div class="card-body">
-<table class="table table-bordered">
-  <tr>
-  	<td><?= $_generate_code ?></td><td><?= $ucode ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_date ?></td><td><?= $udate ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_profile ?></td><td><?= $uprofile ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_validity ?></td><td><?= $uvalid ?></td>
-  <tr>
-  	<td><?= $_time_limit ?></td><td><?= $utlimit ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_data_limit ?></td><td><?= $udlimit ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_price ?></td><td><?= $uprice ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_selling_price ?></td><td><?= $suprice ?></td>
-  </tr>
-  <tr>
-  	<td><?= $_lock_user ?></td><td><?= $ulock ?></td>
-  </tr>
-  <tr>
-    <td colspan="2">
-		<p style="padding:0px 5px;">
-      <?= $_format_time_limit ?>
-    </p>
-    <p style="padding:0px 5px;">
-      <?= $_details_add_user ?>
-    </p>
-    </td>
-  </tr>
-</table>
-</div>
-</div>
-</div>
     <script>
-        // get valid $ price
-        function GetVP() {
-            var prof = document.getElementById('uprof').value;
-            $("#GetValidPrice").load("./process/getvalidprice.php?name=" + prof + "&session=<?= $session; ?> #getdata");
-        }
-
         function setPrefix(select) {
             const option = select.options[select.selectedIndex];
             const prefix = option.getAttribute("data-prefix") || "";
@@ -322,6 +132,5 @@ date_default_timezone_set($_SESSION['timezone']);
             const inputPrefix = document.getElementById("prefix");
             inputPrefix.value = prefix;
         }
-
     </script>
 </div>
--- user-manager/config.php
+++ user-manager/config.php
@@ -7,6 +7,7 @@ return [
             "pass_length" => 4,
             "hotspot_profile" => "test",
             "time_limit" => "4h",
+            "price" => "Rp 2.000",
         ],
         "5.J.R" => [
             "prefix" => "5j",
@@ -14,6 +15,7 @@ return [
             "pass_length" => 4,
             "hotspot_profile" => "voucher-harian",
             "time_limit" => "5h",
+            "price" => "Rp 2.000",
         ],
         "1.B.R" => [
             "prefix" => "",
@@ -21,6 +23,7 @@ return [
             "pass_length" => 4,
             "hotspot_profile" => "voucher-bulanan",
             "time_limit" => "30d",
+            "price" => "Rp 30.000",
         ]
     ]
 ];
